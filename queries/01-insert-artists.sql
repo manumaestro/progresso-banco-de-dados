b33a9408d6612f767f0ac2bb0b3f0d18fea4f652
manumaestro <manuela.maestro@aluno.senai.br>
2025-09-15 14:47:22 -0300

    feat: cria a tabela de artistas

---
+++ queries/01-insert-artists.sql
@@ -0,0 +1,13 @@
+-- Query para criação da tabela --
+
+CREATE TABLE artistas (
+    id SERIAL PRIMARY KEY NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    tipo VARCHAR(20) NOT NULL,
+    membros INTEGER,
+    genero_musical VARCHAR(30),
+    ano_lancamento INTEGER,
+    pais_origem VARCHAR(50),
+    musica_famosa VARCHAR(50),
+    ativo BOOLEAN
+);
